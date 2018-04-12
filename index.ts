4019ddd9e48cdc7c33067990b171c5a2c9bd766a
RubaXa <ibnRubaXa@gmail.com>
2018-04-12 16:18:41 +0300

    + export getSpyChain

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import spy from './src/spy/spy';
+import spy, {getSpyChain} from './src/spy/spy';
 import {intercept, UNCAUGHT} from './src/intercept/intercept';
 import {
 	addSpyObserver,
@@ -11,6 +11,7 @@ import Spy from './src/component/component';
 export {
 	spy,
 	Spy,
+	getSpyChain,
 	intercept,
 	addSpyObserver,
 	addSpyErrorObserver,
